99108eb11d5c2e06e1b079f09e9906e48cc2ee72
240001268 <240001268@esg.ipsantarem.pt>
2025-03-18 23:05:30 +0000

    Add files via upload

---
+++ Esfera.java
@@ -0,0 +1,31 @@
+
+public class Esfera {
+
+	//1 - Atributos
+	private double raio;
+	
+	//2 - Construtores
+	
+	public Esfera(double raio) {
+		if (raio<=0) {
+			System.out.println("Valor inválido");
+		}
+		else {
+			this.raio = raio; 
+		}
+	}
+	//3 - Acessores (gets & sets)
+	
+	public double getRaio() {
+		return raio;
+	}
+	
+	
+	//4 - Comportamentos
+	
+	
+	
+	//5 - Metodos complementares
+
+	
+}
